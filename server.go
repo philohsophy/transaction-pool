2fe9fc8fab3b65664221d4e680e0501384728d9b
Phil <phil.dev@mailbox.org>
2021-04-11 21:50:46 +0200

    Using mux

--- server.go
+++ server.go
@@ -6,21 +6,25 @@ import (
 	"net/http"
 )
 
-func init() {
-	log.Println("server package initialized")
-}
+type myHandler struct{}
 
-func main() {
-	// Set routing rules
-	http.HandleFunc("/", Tmp)
-
-	//Use the default DefaultServeMux.
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+func (*myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "URL: "+r.URL.String())
 }
 
 func Tmp(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "version 1")
+	io.WriteString(w, "version 2")
+}
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.Handle("/", &myHandler{})
+
+	mux.HandleFunc("/tmp", Tmp)
+
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
