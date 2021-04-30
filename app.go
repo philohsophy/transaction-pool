2818539d7466d38607c16f43c94b3d3e96ba4400
Phil <phil.dev@mailbox.org>
2021-04-30 21:16:12 +0200

    - fixed error handling for createTransaction - added logging output

--- app.go
+++ app.go
@@ -9,7 +9,7 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/gorilla/mux"
-	_ "github.com/lib/pq"
+	"github.com/lib/pq"
 )
 
 type App struct {
@@ -19,25 +19,50 @@ type App struct {
 
 func (a *App) Initialize(user, password, dbname string) {
 	connectionString := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", user, password, dbname)
-
-	var err error
-	a.DB, err = sql.Open("postgres", connectionString)
-	if err != nil {
-		log.Fatal(err)
-	}
+	a.connectToDatabase(connectionString)
+	a.initializeDatabase()
 
 	a.Router = mux.NewRouter()
 	a.initializeRoutes()
 }
 
+func (a *App) connectToDatabase(connectionString string) {
+	fmt.Print("Connecting to database...")
+	var err error
+	a.DB, err = sql.Open("postgres", connectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(" ok")
+}
+
+const tableCreationQuery = `CREATE TABLE IF NOT EXISTS transactions
+(
+	id UUID PRIMARY KEY,
+	recipient_address JSONB NOT NULL,
+	sender_address JSONB NOT NULL,
+	value NUMERIC(10,2) NOT NULL DEFAULT 0.00
+)`
+
+func (a *App) initializeDatabase() {
+	fmt.Print("Initializing database...")
+	if _, err := a.DB.Exec(tableCreationQuery); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(" ok")
+}
+
 func (a *App) initializeRoutes() {
+	fmt.Print("Initializing routes...")
 	a.Router.HandleFunc("/transactions", a.getTransactions).Methods("GET")
 	a.Router.HandleFunc("/transactions", a.createTransaction).Methods("POST")
 	a.Router.HandleFunc("/transactions/{id:[a-z0-9-]+}", a.getTransaction).Methods("GET")
 	a.Router.HandleFunc("/transactions/{id:[a-z0-9-]+}", a.deleteTransaction).Methods("DELETE")
+	fmt.Println(" ok")
 }
 
 func (a *App) Run(addr string) {
+	log.Printf("Serving on %s", addr)
 	log.Fatal(http.ListenAndServe(addr, a.Router))
 }
 
@@ -87,9 +112,11 @@ func (a *App) createTransaction(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 
 	if err := t.createTransaction(a.DB); err != nil {
-		switch err {
-		case err.(*InvalidTransactionError):
+		switch err.(type) {
+		case *InvalidTransactionError:
 			respondWithError(w, http.StatusBadRequest, err.Error())
+		case *pq.Error:
+			respondWithError(w, http.StatusInternalServerError, err.Error())
 		default:
 			respondWithError(w, http.StatusInternalServerError, err.Error())
 		}
